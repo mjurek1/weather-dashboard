5c8fbf7ec8c0b73b5ec4640603d7eb2e0f628d42
mjurek1 <jurekmichael0@gmail.com>
2024-09-11 21:23:41 -0500

    connecting API

---
+++ script.js
@@ -0,0 +1,7 @@
+const city = document.querySelector(".city");
+const searchButton = document.querySelector(".search-btn");
+const currentWeather = document.querySelector(".current-weather");
+const forecastCards = document.querySelector(".forecast-cards");
+
+const API_KEY = "9f42ae0bc80c68b5f45e59a1379a6d6c";
+
