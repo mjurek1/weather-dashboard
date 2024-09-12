0a2eba8bcd124763e84b68f1eda3b37f9b76bbf2
mjurek1 <jurekmichael0@gmail.com>
2024-09-11 23:37:01 -0500

    updating dashabord now that its working with API calls

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# weather-dashboard
-server side APIs weather dashboard
+# Weather Dashboard
+
+Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Our challenge this week is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. 
+
+## Usage
+The following use cases were addressed as part of this project
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+with the following Acceptance Criteria
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- script.js
+++ script.js
@@ -5,3 +5,62 @@ const forecastCards = document.querySelector(".forecast-cards");
 
 const API_KEY = "9f42ae0bc80c68b5f45e59a1379a6d6c";
 
+const createForecastCard = (cityName, weatherItem, index) => {
+    if(index === 0) {
+        return `<div class="details">
+        <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+        <h3>Temperature: ${Math.round((weatherItem.main.temp - 273.15) * 9/5 + 32)} F</h3>
+        <h3>Wind: ${weatherItem.wind.speed} mph</h3>
+        <h3>Humidity: ${weatherItem.main.humidity} %</h3> 
+        </div>;`
+    } else {
+        return `<li class = "card"
+        <h2>(${weatherItem.dt_txt.split(" ")[0]})</h2>
+        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
+        <h3>Temperature: ${Math.round((weatherItem.main.temp - 273.15) * 9/5 + 32)} F</h3>
+        <h3>Wind: ${weatherItem.wind.speed} mph</h3>
+        <h3>Humidity: ${weatherItem.main.humidity} %</h3> 
+        </li>`; 
+    }
+}
+
+const getWeatherInfo = (cityName, lat, lon) => {
+    const API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+    fetch(API_URL).then(res => res.json()).then(data => {
+
+        const forecastDay = [];
+        const fiveDayForecast = data.list.filter(forecast => {
+            const forecastData = new Date(forecast.dt_txt).getDate();
+            if(!forecastDay.includes(forecastData)) {
+                return forecastDay.push(forecastData);
+            }
+        });
+        city.value = "";
+        currentWeather.innerHTML = "";
+        forecastCards.innerHTML = "";
+
+        fiveDayForecast.forEach((weatherItem, index) => {
+            if(index === 0) {
+                currentWeather.insertAdjacentHTML("beforeend", createForecastCard(cityName, weatherItem, index));
+            } else {
+                forecastCards.insertAdjacentHTML("beforeend", createForecastCard(cityName, weatherItem, index));
+            }
+        })
+    });
+    }
+
+    const getCityCoords = () => {
+        const cityName = city.value.trim();
+        if(!cityName) return;
+        const pikachu =  `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+        fetch(pikachu).then(res => res.json()).then(data => {
+            if(!data.length) return alert(`couldn't find ${cityName}`);
+        const {name, lat, lon} = data[0];
+        getWeatherInfo(name, lat, lon);
+        }).catch(() => {
+            alert("unable to get coordinates");
+        });
+    }
+
+    searchButton.addEventListener("click", getCityCoords);
